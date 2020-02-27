7348cb39a760160f590691e35ac4e7327edbe4bc
ellielove <ellie@pacifist.studio>
2020-02-27 01:31:57 -0800

    Create entry_point.cpp
    entry point for engine

---
+++ Witchcraft/Witchcraft/entry_point.cpp
@@ -0,0 +1,30 @@
+#include <windows.h>
+
+#include "error_log_manager.h"
+
+const std::string ErrorLogFileName = "witchcraft_error_log.txt";
+
+int main(int argc, char** argv[])
+{
+	OutputDebugString("\n\nEntry Point for program\n\n");
+
+	cErrorLogManager * log = cErrorLogManager::GetErrorManager();
+	log->createErrorLog(ErrorLogFileName);
+
+	try
+	{
+		THROW_EXCEPTION(666, "Ellie loves satan");
+	}
+	catch (cException & e)
+	{
+		MessageBoxA(NULL, e.what(), "", MB_OK);
+		log->m_LogBuffer << "\n\n*************** ERROR ***************\n";
+		log->flushBuffer();
+		log->logException(e);
+		log->m_LogBuffer << "\n\n*************************************";
+		log->flushBuffer();
+	}
+
+	log->closeFile();
+	
+}

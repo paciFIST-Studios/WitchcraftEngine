bc322f8624ade35cae95ef223b9a1cd01583b98f
ellielove <ellie@pacifist.studio>
2020-02-28 21:46:31 -0800

    Update entry_point.cc
    Update entry point to run tests when the program starts.  This is not final configuration, but we'll leave it this way for a while.
    
    Co-Authored-By: ellielove <ellielove@users.noreply.github.com>
    Co-Authored-By: paciFIST Studios <pacifist-studios@users.noreply.github.com>

--- Witchcraft/Witchcraft/entry_point.cc
+++ Witchcraft/Witchcraft/entry_point.cc
@@ -1,4 +1,12 @@
 
+#include "unit_testing_switch.h"
+#ifdef RUN_UNIT_TESTS
+	#define CATCH_CONFIG_RUNNER
+	#include "catch.hpp"
+#endif // RUN_UNIT_TESTS
+
+
+#include <iostream>
 
 #include "utility\utility.h"
 #include "logging\error_log_manager.h"
@@ -8,26 +16,29 @@ const std::string ErrorLogFileName = "witchcraft_error_log.txt";
 
 int main(int argc, char** argv[])
 {
-	utility::WriteConsole("\n\nTEST\n\n");
+	Catch::Session session;
+	int numberOfFailures = session.run();
 
-	
-	cErrorLogManager * log = cErrorLogManager::GetErrorManager();
-	log->createErrorLog(ErrorLogFileName);
-	
-	try
-	{
-		THROW_EXCEPTION(666, "Ellie loves satan");
-	}
-	catch (cException & e)
-	{
-		MessageBoxA(NULL, e.what(), "", MB_OK);
-		log->m_LogBuffer << "\n\n*************** ERROR ***************\n";
-		log->flushBuffer();
-		log->logException(e);
-		log->m_LogBuffer << "\n\n*************************************";
-		log->flushBuffer();
-	}
-	
-	log->closeFile();
+	char c;
+	std::cin >> c;
+
+	//cErrorLogManager * log = cErrorLogManager::GetErrorManager();
+	//log->createErrorLog(ErrorLogFileName);
+	//
+	//try
+	//{
+	//	THROW_EXCEPTION(666, "Ellie loves satan");
+	//}
+	//catch (cException & e)
+	//{
+	//	MessageBoxA(NULL, e.what(), "", MB_OK);
+	//	log->m_LogBuffer << "\n\n*************** ERROR ***************\n";
+	//	log->flushBuffer();
+	//	log->logException(e);
+	//	log->m_LogBuffer << "\n\n*************************************";
+	//	log->flushBuffer();
+	//}
+	//
+	//log->closeFile();
 	
 }

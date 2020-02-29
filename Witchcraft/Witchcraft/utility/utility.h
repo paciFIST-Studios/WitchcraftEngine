641cac037ec19089b165ddfca7974ec0ce5bc70b
ellielove <ellie@pacifist.studio>
2020-02-28 21:19:54 -0800

    Update utility.h
    Added
    
    * static std::string utility::readFileToString(std::string const & path)
        - Reads a txt file and gives you a string
        - this needs tests
    
    Co-Authored-By: ellielove <ellielove@users.noreply.github.com>
    Co-Authored-By: paciFIST Studios <pacifist-studios@users.noreply.github.com>

--- Witchcraft/Witchcraft/utility/utility.h
+++ Witchcraft/Witchcraft/utility/utility.h
@@ -1,34 +1,54 @@
 #ifndef UTILITY_H
 #define UTILITY_H
 
-#include <Windows.h>
+	#include <Windows.h>
 
-#include <iostream>
-#include <sys/stat.h>
-#include <string>
+	#include <iostream>
+	#include <sys/stat.h>
+	#include <string>
 
-// [NOTE] I read that putting the static methods in a class is "wrong" for C++,
-// so instead we're going to use namespaces
-namespace utility
-{
-	static bool fileExists(std::string const & path)
+	// [NOTE] I read that putting the static methods in a class is "wrong" for C++,
+	// so instead we're going to use namespaces
+	namespace utility
 	{
-		struct stat buffer;
-		return (stat(path.c_str(), &buffer) == 0);
-	}
+		static std::string readFileToString(std::string const & path)
+		{
+			// https://stackoverflow.com/questions/116038/how-do-i-read-an-entire-file-into-a-stdstring-in-c
+			std::ifstream infile(path.c_str(), std::ios::in | std::ios::binary | std::ios::ate);			
+			auto file_size = infile.tellg();
 
-	static void WriteConsole(std::string message)
-	{
-		std::cout << message;
+			// We can get back a -1 as an error code, and this will cause
+			// us to allocate like, INT_MAX bytes for the string (from implicit casting)
+			// return a default string instead
+			if (file_size < 0)
+				return std::string();
+			
+			infile.seekg(0, std::ios::beg);
+			std::vector<char> bytes(file_size);
+			infile.read(bytes.data(), file_size);
+			infile.close();
 
-		// This preprocessor macro is used to send the debug string
-		// to VS's output window.  The message already goes to stdout
-		// We use the macro to make sure this code isn't even in the
-		// release build
-		#ifdef _DEBUG
-			OutputDebugString(message.c_str());
-		#endif
+			return std::string(bytes.data(), file_size);
+		}
+
+		static bool fileExists(std::string const & path)
+		{
+			struct stat buffer;
+			return (stat(path.c_str(), &buffer) == 0);
+		}
+
+		static void WriteToConsole(std::string message)
+		{
+			std::cout << message;
+
+			// This preprocessor macro is used to send the debug string
+			// to VS's output window.  The message already goes to stdout
+			// We use the macro to make sure this code isn't even in the
+			// release build
+			#ifdef _DEBUG
+				OutputDebugString(message.c_str());
+			#endif
+		}
 	}
-}
 
 #endif

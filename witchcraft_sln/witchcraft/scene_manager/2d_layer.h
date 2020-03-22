bbe92a71e7246e5b4ec59c587bde0ecc27a20d33
ellielove <ellie@pacifist.studio>
2020-03-22 01:23:19 -0700

    2DLayer
    1. update to header
    2. update to tests
    
    Notes:
    Have included some constants for defualt values, but could not think of a really good way to deal with the string constant.  Right now, we're jsut going to have to be consistent on what our initialization default values are.

--- witchcraft_sln/witchcraft/scene_manager/2d_layer.h
+++ witchcraft_sln/witchcraft/scene_manager/2d_layer.h
@@ -1,9 +1,41 @@
 #ifndef TWO_D_LAYER_H
 #define TWO_D_LAYER_H
 
-class c2DLayer
-{
+#include <string>
 
+#include "../engine/engine.h"
+#include "scene_object.h"
+
+class c2DLayer : public cEngineObject
+{
+private:
+protected:
+public:
+
+	static bool			constexpr UNINIT_VISIBILITY		= false;
+	static unsigned int constexpr UNINIT_Z_ORDER		= 0;
+	static float		constexpr UNINIT_COORDINATE_F	= 0.0;
+
+	c2DLayer()
+		: _layer_is_visible(UNINIT_VISIBILITY)
+		, _layer_z_order(UNINIT_Z_ORDER)
+		, _position_x(UNINIT_COORDINATE_F)
+		, _position_y(UNINIT_COORDINATE_F)
+		, _name{std::string("un-init")}
+	{
+	}
+
+	bool _layer_is_visible;
+	unsigned int _layer_z_order;
+
+	float _position_x;
+	float _position_y;
+
+	std::string _name;
+
+	std::vector<std::unique_ptr<cSceneObject>> _scene_objects;
+
+	void update();
 };
 
 #endif // !TWO_D_LAYER_H
--- witchcraft_sln/witchcraft/scene_manager/test_2d_layer.cc
+++ witchcraft_sln/witchcraft/scene_manager/test_2d_layer.cc
@@ -9,9 +9,19 @@
 		
 		#include "2d_layer.h"
 		
-		TEST_CASE("c2DLayer::c2DLayer()")
+		TEST_CASE("c2DLayer::ctor")
 		{
-			REQUIRE_NOTHROW(c2DLayer());
+			c2DLayer layer;
+
+			// ctor can construct
+			REQUIRE_NOTHROW(layer = c2DLayer());
+
+			// and default ctor uses un-init values
+			REQUIRE(layer._layer_z_order == c2DLayer::UNINIT_Z_ORDER);
+			REQUIRE(layer._layer_is_visible == c2DLayer::UNINIT_VISIBILITY);
+			REQUIRE(layer._position_x == c2DLayer::UNINIT_COORDINATE_F);
+			REQUIRE(layer._position_y == c2DLayer::UNINIT_COORDINATE_F);
+			REQUIRE(layer._name == "un-init");
 		}
 
 	#endif // RUN_UNIT_TESTS

d17b602c2c2f9b0896e552b4f1368128624b761f
ellielove <ellie@pacifist.studio>
2020-03-22 02:13:14 -0700

    cTimer
    1. update header
    2. update tests
    
    3. new dependency winmm.lib
    
    We call timeGetTime(), and this is a windows system call, requiring us to link against the(a?) windows system library, so we can call their api

--- witchcraft_sln/witchcraft/scene_manager/test_timer.cc
+++ witchcraft_sln/witchcraft/scene_manager/test_timer.cc
@@ -11,8 +11,41 @@
 		
 		TEST_CASE("cTimer::cTimer()")
 		{
-			REQUIRE_NOTHROW(cTimer());
+			cTimer * ptr;
+			REQUIRE_NOTHROW(ptr = &cTimer());
+
+			REQUIRE(ptr != nullptr);
+
+			REQUIRE(ptr->_id == cTimer::UNINIT_ID);
+			REQUIRE(ptr->_start_time == cTimer::UNINIT_START_TIME);
+			REQUIRE(ptr->_interval_length == cTimer::UNINIT_INTERVAL_LEN);
+			REQUIRE(ptr->_time_period_elapsed == cTimer::UNINIT_TIME_ELAPSED);
 		}
+
+		TEST_CASE("cTimer::start()")
+		{
+			// can call start method
+			auto t1 = cTimer();
+			REQUIRE_NOTHROW(t1.start());
+		}
+
+		TEST_CASE("cTimer::update()")
+		{
+			auto t3 = &cTimer();
+			t3->_interval_length = 100; // ms
+
+			t3->start();
+			for (int i = 0; i < 100; i++)
+			{
+				t3->update();
+				Sleep(25);
+
+				if (t3->_time_period_elapsed)
+					break;
+			}
+			REQUIRE(t3->_time_period_elapsed);
+		}
+
 
 	#endif // RUN_UNIT_TESTS
 
--- witchcraft_sln/witchcraft/scene_manager/timer.h
+++ witchcraft_sln/witchcraft/scene_manager/timer.h
@@ -1,9 +1,51 @@
 #ifndef TIMER_H
 #define TIMER_H
 
+#include <time.h>
+#include <windows.h>
+
+
 class cTimer
 {
+private:
+protected:
 
+public:
+	static unsigned int constexpr UNINIT_ID = 0;
+	static DWORD constexpr UNINIT_START_TIME = 0;
+	static DWORD constexpr UNINIT_INTERVAL_LEN = 0;
+	static bool constexpr UNINIT_TIME_ELAPSED = true;
+
+	unsigned int _id;
+	DWORD _start_time;			// ms (miliseconds)
+	DWORD _interval_length;		// ms
+	bool _time_period_elapsed;
+
+	cTimer()
+		: _id(UNINIT_ID)
+		, _start_time(UNINIT_START_TIME)
+		, _interval_length(UNINIT_INTERVAL_LEN)
+		, _time_period_elapsed(UNINIT_TIME_ELAPSED)
+	{}
+
+	void start()
+	{
+		_start_time = timeGetTime();
+		_time_period_elapsed = false;
+	}
+
+	void update()
+	{
+		if (_time_period_elapsed)
+			return;
+
+		DWORD elapsed_time = timeGetTime() - _start_time;
+
+		if (elapsed_time > _interval_length)
+		{
+			_time_period_elapsed = true;
+		}
+	}
 };
 
 #endif // !TIMER_H

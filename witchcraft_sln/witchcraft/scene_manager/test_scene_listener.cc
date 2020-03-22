22bf868a92f857644d9f94c7099ac70796c449f4
ellielove <ellie@pacifist.studio>
2020-03-21 23:10:48 -0700

    scene_listener tests
    1. I don't know how to test an abstract base class right

--- witchcraft_sln/witchcraft/scene_manager/test_scene_listener.cc
+++ witchcraft_sln/witchcraft/scene_manager/test_scene_listener.cc
@@ -8,11 +8,48 @@
 		#include "../catch.hpp"
 		
 		#include "scene_listener.h"
-		
-		TEST_CASE("cSceneListener::cSceneListener()")
+	
+		class TestSceneListener : public cSceneListener
+		{		
+			void event(c2DSceneManager const & manager, void * callback_data) const override
+			{
+			}
+		};
+
+		TEST_CASE("TestSceneListener::ctor")
+		{
+			// We should be able to override an ABC w/o causing problems
+			REQUIRE_NOTHROW(TestSceneListener());
+		}
+
+
+		// NOTES: This is an abstract base class (It's meant to be), so we can't
+		// really run a test on it, because: 
+		//		if it remains an abstract base class, it will throw a compiler error
+		//		if you ever try to instantiate it.
+		//
+		//		as a result, if we write the code that will test instantiation, then
+		//		we are also writing code that cannot compile.
+		//		it would work as a test, ish, but it would also prevent the project from	
+		//		being compiled, and that would cause the project to fail.  
+		//
+		//		So for right now, to run the test, you manually comment-in the 
+		//			#define FAIL_COMPILATION
+		//		line, and then you try to compile.  If the compilation fails, then the 
+		//		test works but also you'll have to comment out the #define again, 
+		//		before you can actually compile the project.
+		//
+		// the result is, this kind of has to be a manual test. finding an automated mechanism
+		// is a low priority
+
+		//#define FAIL_COMPILATION
+
+		#ifdef FAIL_COMPILATION
+		TEST_CASE("cSceneListener::cSceneListener() compilation fails for abstract base class")
 		{
 			REQUIRE_NOTHROW(cSceneListener());
 		}
+		#endif // !FAIL_COMPILATION
 
 	#endif // RUN_UNIT_TESTS
 

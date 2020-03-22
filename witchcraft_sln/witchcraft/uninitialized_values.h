77a0412b83a9cb930d4a90d2768d27bba9f168da
ellielove <ellie@pacifist.studio>
2020-03-22 04:13:01 -0700

    Create uninitialized_values.h
    New Standard:
    
    1. when creating any object, the user must supply all values for creation of the object
    2. Otherwise, the default ctor is to place "uninitialized values" in each member variable.
    
    The values go in this file

---
+++ witchcraft_sln/witchcraft/uninitialized_values.h
@@ -0,0 +1,18 @@
+#ifndef UNINITIALIZED_VALUES_H
+#define UNINITIALIZED_VALUES_H
+
+
+
+namespace init
+{
+	static bool  constexpr UNI_BOOL = false;
+
+	static float constexpr UNI_FLOAT = 0.0;
+
+	static unsigned int UNI_UINT = 0;
+
+	static unsigned char UNI_UCHAR = 0;
+}
+
+
+#endif // !UNINITIALIZED_VALUES_H

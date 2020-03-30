f897ec39c3c4ddf5adad07173f612c2c044f8e40
ellielove <ellie@pacifist.studio>
2020-03-30 12:20:02 -0700

    Engine and tests
    Updating engine state enum, and also adding a getter for accessing it in tests

--- witchcraft_sln/witchcraft/engine/engine.h
+++ witchcraft_sln/witchcraft/engine/engine.h
@@ -9,6 +9,17 @@ class cResourceManager;
 class cRenderManager;
 class cSceneManager;
 
+enum class EEngineState : unsigned char
+{
+	  UNINIT		= 0x01
+	, CONSTRUCTED	= 0x02
+	, STARTUP		= 0x04
+	, RUNNING		= 0x08
+	, SHUTDOWN		= 0x10
+	// 0x20
+	// 0x40
+	// 0x80
+};
 
 struct EngineInitializer
 {
@@ -25,14 +36,22 @@ protected:
 	//std::unique_ptr<cRenderManager> render;
 	//std::unique_ptr<cSceneManager> scene;
 
+	EEngineState _current_engine_state = EEngineState::UNINIT;
+
 public:
 	
 	void startup();
 	void run();
 	void shutdown();
 
-	cEngine() {}
+	EEngineState get_current_state() const { return _current_engine_state; }
+
+	cEngine() 
+		: _current_engine_state(EEngineState::CONSTRUCTED) 
+	{}
+
 	cEngine(EngineInitializer init) 
+		: _current_engine_state(EEngineState::CONSTRUCTED)
 	{
 		id = init.id;
 	}
--- witchcraft_sln/witchcraft/engine/engine__tst.cc
+++ witchcraft_sln/witchcraft/engine/engine__tst.cc
@@ -16,12 +16,42 @@
 			// default init 
 			auto engine = cEngine();
 			REQUIRE(engine.id == uninit::UINT);
+			REQUIRE(engine.get_current_state() == EEngineState::CONSTRUCTED);
+
 
 			// init with initializer should give the correct id
-			auto einit = EngineInitializer{ 666 };
-			auto engine2 = cEngine(einit);
+			auto init = EngineInitializer{ 666 };
+			auto engine2 = cEngine(init);
 			REQUIRE(engine2.id == 666);
+			REQUIRE(engine.get_current_state() == EEngineState::CONSTRUCTED);
 
+		}
+
+		TEST_CASE("cEngine::startup()")
+		{
+			auto init = EngineInitializer{ 1 };
+			auto engine = cEngine(init);
+
+			engine.startup();
+			REQUIRE(engine.get_current_state() == EEngineState::STARTUP);
+		}
+
+		TEST_CASE("cEngine::run()")
+		{
+			auto init = EngineInitializer{ 1 };
+			auto engine = cEngine(init);
+
+			engine.run();
+			REQUIRE(engine.get_current_state() == EEngineState::RUNNING);
+		}
+
+		TEST_CASE("cEngine::shutdown()")
+		{
+			auto init = EngineInitializer{ 1 };
+			auto engine = cEngine(init);
+
+			engine.shutdown();
+			REQUIRE(engine.get_current_state() == EEngineState::SHUTDOWN);
 		}
 
 	#endif // RUN_UNIT_TESTS

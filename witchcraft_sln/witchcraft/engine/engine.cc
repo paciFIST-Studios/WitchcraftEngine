3ee480ce2c93a3c3deec719910ad330f0a5934bc
ellielove <ellie@pacifist.studio>
2020-03-30 12:22:28 -0700

    Update engine.cc
    all methods cause change to that state, order is not restricted

--- witchcraft_sln/witchcraft/engine/engine.cc
+++ witchcraft_sln/witchcraft/engine/engine.cc
@@ -3,15 +3,32 @@
 
 void cEngine::startup()
 {
+	PLOGI << witchcraft::log_strings::engine_startup;
+	_current_engine_state = EEngineState::STARTUP;
+
+	PLOGI << witchcraft::log_strings::resource_manager_start;
+	resource = std::make_unique<cResourceManager>();
+	//witchcraft::configuration::create_config_files();
+	//resource->load_from_xml_file(witchcraft::configuration::birds_asset);
+	//resource->load_from_xml_file(witchcraft::configuration::buddha_asset);
+	//resource->load_from_xml_file(witchcraft::configuration::person_asset);
+
 
 }
 
 void cEngine::run()
 {
+	PLOGI << witchcraft::log_strings::engine_running;
+	_current_engine_state = EEngineState::RUNNING;
+
+
 
 }
 
 void cEngine::shutdown()
 {
+	PLOGI << witchcraft::log_strings::engine_shutdown;
+	_current_engine_state = EEngineState::SHUTDOWN;
+
 
 }

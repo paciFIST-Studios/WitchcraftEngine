b25f51db76e2bebfe7d666e16db31dd911982fba
ellielove <ellie@pacifist.studio>
2020-03-29 22:22:27 -0700

    Update engine.h
    mostly updating whitespace

--- witchcraft_sln/witchcraft/engine/engine.h
+++ witchcraft_sln/witchcraft/engine/engine.h
@@ -1,20 +1,24 @@
 #ifndef ENGINE_H
 #define ENGINE_H
 
-class cEngineObject
-{
-private:
-protected:
-public:
-	int _id; // unique
-};
+#include "../engine/engine_object.h"
 
-class cEngine
+class cEngine : public cEngineObject
 {
 private:
 protected:
 public:
-	int _id; // unique
+
+	//std::unique_ptr<cResourceManager> resource;
+	//std::unique_ptr<cRenderManager> render;
+	//std::unique_ptr<cSceneManager> scene;
+	//std::unique_ptr<cMessageManager> message;
+
+	void startup();
+	void run();
+	void shutdown();
+
+	cEngine() {}
 };
 
 #endif // !ENGINE_H

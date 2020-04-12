808a343edf327eaca360d99fae849ceb339bd7c0
ellielove <ellie@pacifist.studio>
2020-04-11 23:54:37 -0700

    Update Layer2D

--- witchcraft_sln/witchcraft/scene_manager/layer_2d.h
+++ witchcraft_sln/witchcraft/scene_manager/layer_2d.h
@@ -6,31 +6,76 @@
 #include "../engine/engine_object.h"
 #include "scene_object.h"
 
+
+struct Layer2DInitializer
+{
+	unsigned int ID;
+	unsigned int zOrder;
+	float x;
+	float y;
+	bool is_visible;
+	std::string name;
+};
+
 class Layer2D : public qEngineObject
 {
 private:
 protected:
+	std::string const name;
+
+	unsigned int z_order;
+
+	bool visible;
+
+	float pos_x;
+	float pos_y;
+
+
+	std::vector<std::unique_ptr<qSceneObject>> scene_objects;
+
+
 public:
 
 	Layer2D()
-		: _layer_is_visible(uninit::BOOL)
-		, _layer_z_order(uninit::UINT)
-		, position_x_(uninit::FLOAT)
-		, position_y_(uninit::FLOAT)
-		, _name{std::string(uninit::CSTRING)}
+		: qEngineObject()
+		, name{ std::string(uninit::CSTRING) }
+		, z_order(uninit::UINT)
+		, visible(uninit::BOOL)
+		, pos_x(uninit::FLOAT)
+		, pos_y(uninit::FLOAT)
 	{}
 
-	bool _layer_is_visible;
-	unsigned int _layer_z_order;
+	Layer2D(Layer2DInitializer init)
+		: qEngineObject(init.ID)
+		, z_order(init.zOrder)
+		, visible(init.is_visible)
+		, pos_x(init.x)
+		, pos_y(init.y)
+		, name(init.name)
+	{}
+	
+	~Layer2D() {}
+	
 
-	float position_x_;
-	float position_y_;
-
-	std::string _name;
-
-	std::vector<std::unique_ptr<cSceneObject>> _scene_objects;
-
+	
 	void update();
+
+
+
+	// vis
+	bool get_is_visible() const { return visible; }
+	void set_is_visible(bool is) { visible = is; }
+
+	// z order
+	unsigned int get_z_order() const { return z_order; }
+	void set_z_order(unsigned int z) { z_order = z; }
+
+	// xy pos of layer itself
+	std::tuple<float, float> get_offset() const { return std::make_tuple(pos_x, pos_y); }
+	void set_offset(float x, float y) { pos_x = x; pos_y = y; }
+
+	std::string get_name() const { return name; }
+
 };
 
 #endif // !LAYER_TWO_D_H
--- witchcraft_sln/witchcraft/scene_manager/layer_2d__tst.cc
+++ witchcraft_sln/witchcraft/scene_manager/layer_2d__tst.cc
@@ -9,19 +9,90 @@
 		
 		#include "layer_2d.h"
 		
-		TEST_CASE(" Layer2D::ctor")
+		TEST_CASE(" Layer2D::ctor::uninit")
 		{
-			Layer2D layer;
-
-			// ctor can construct
-			REQUIRE_NOTHROW(layer = Layer2D());
+			Layer2D layer = Layer2D();
 
 			// and default ctor uses un-init values
-			REQUIRE(layer._layer_z_order == uninit::UINT);
-			REQUIRE(layer._layer_is_visible == uninit::BOOL);
-			REQUIRE(layer.position_x_ == uninit::FLOAT);
-			REQUIRE(layer.position_y_ == uninit::FLOAT);
-			REQUIRE(layer._name == std::string(uninit::CSTRING));
+			REQUIRE(layer.get_z_order() == uninit::UINT);
+			REQUIRE(layer.get_is_visible() == uninit::BOOL);
+
+			auto tuple = layer.get_offset();
+			REQUIRE(std::get<0>(tuple) == uninit::FLOAT);
+			REQUIRE(std::get<1>(tuple) == uninit::FLOAT);
+			REQUIRE(layer.get_name() == std::string(uninit::CSTRING));
+		}
+
+		TEST_CASE(" Layer2D::ctor")
+		{
+			unsigned int const test_id = 666;
+			bool const test_visibility = true;
+			std::string const test_name = "background_1";
+			float const test_x = 10;
+			float const test_y = 12;
+			unsigned int const test_z_order = 1;
+
+			Layer2DInitializer init;
+			init.ID = test_id;
+			init.is_visible = test_visibility;
+			init.name = test_name;
+			init.x = test_x;
+			init.y = test_y;
+			init.zOrder = test_z_order;
+
+			auto layer = Layer2D(init);
+
+			REQUIRE(layer.id == test_id);
+			REQUIRE(layer.get_is_visible() == test_visibility);
+			REQUIRE(layer.get_name() == test_name);
+
+			auto tuple = layer.get_offset();
+			REQUIRE(std::get<0>(tuple) == test_x);
+			REQUIRE(std::get<1>(tuple) == test_y);
+
+			REQUIRE(layer.get_z_order() == test_z_order);
+		}
+
+		TEST_CASE(" Layer2D:: is_visible")
+		{
+			bool test_value = false;
+			auto layer = Layer2D();
+
+			layer.set_is_visible(test_value);
+			REQUIRE(layer.get_is_visible() == test_value);
+
+			layer.set_is_visible(!test_value);
+			REQUIRE(layer.get_is_visible() == !test_value);
+		}
+
+		TEST_CASE(" Layer2D:: z_order")
+		{
+			unsigned int const test_value = 666;
+			auto layer = Layer2D();
+			
+			REQUIRE(layer.get_z_order() == uninit::UINT);
+			layer.set_z_order(test_value);
+			REQUIRE(layer.get_z_order() == test_value);
+			layer.set_z_order(test_value + 1);
+			REQUIRE(layer.get_z_order() == test_value + 1);
+		}
+
+		TEST_CASE(" Layer2D:: offset")
+		{
+			float const x = 1;
+			float const y = 2;
+
+			auto layer = Layer2D();
+
+			layer.set_offset(x, y);
+			auto tuple = layer.get_offset();
+			REQUIRE(std::get<0>(tuple) == x);
+			REQUIRE(std::get<1>(tuple) == y);
+
+			layer.set_offset(x+1, y+1);
+			auto tuple2 = layer.get_offset();
+			REQUIRE(std::get<0>(tuple2) == x+1);
+			REQUIRE(std::get<1>(tuple2) == y+1);
 		}
 
 	#endif // RUN_UNIT_TESTS

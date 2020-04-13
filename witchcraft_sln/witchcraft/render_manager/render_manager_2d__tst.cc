e9001c4dac9e9b7169d2d49473da6fd9d89d15c9
ellielove <ellie@pacifist.studio>
2020-04-13 02:44:07 -0700

    Update render_manager_2d__tst.cc
    we'll have to redo these tests

--- witchcraft_sln/witchcraft/render_manager/render_manager_2d__tst.cc
+++ witchcraft_sln/witchcraft/render_manager/render_manager_2d__tst.cc
@@ -21,47 +21,10 @@
 		class TestRenderManager2D : public RenderManager2D
 		{
 		public:
-			witchcraft::callback_types::SceneLayersCallbackType2 get_stored_cb() const
-			{
-				return cb_get_layers;
-			}
-
-			bool use_stored_cb()
-			{
-				if (cb_get_layers == nullptr)
-				{
-					return false;
-				}
-
-				auto ret = cb_get_layers();
-				auto ret_ptr = &ret;
-				if (ret_ptr == nullptr)
-				{
-					return false;
-				}
-
-				return true;
-			}
 		};
 		
-		TEST_CASE(" RenderManager2D:: set_scene_layers_callback integration and use")
+		TEST_CASE(" RenderManager2D:: ")
 		{
-			auto rm = TestRenderManager2D();
-			auto sm = SceneManager2D();
-
-			for (int i = 0; i < 5; i++)
-			{
-				sm.add_layer("test_" + std::to_string(i));
-			}
-
-			auto scene_cb = sm.get_scene_layers_callback();
-			rm.set_scene_layers_callback(scene_cb);
-
-			auto cb = rm.get_stored_cb();
-			REQUIRE(cb != nullptr);
-
-			// returns true if the cb was called, and then we got the return value
-			REQUIRE(rm.use_stored_cb());
 		}
 
 	#endif // RUN_UNIT_TESTS

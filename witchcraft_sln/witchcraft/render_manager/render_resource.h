8533772ba09b9c7aff55f79f2c584b2c5cee477d
ellielove <ellie@pacifist.studio>
2020-08-10 11:36:28 -0700

    Update render_resource.h
    Putting this back the way it was

--- witchcraft_sln/witchcraft/render_manager/render_resource.h
+++ witchcraft_sln/witchcraft/render_manager/render_resource.h
@@ -1,7 +1,6 @@
 #ifndef RENDER_RESOURCE_H
 #define RENDER_RESOURCE_H
 
-#include <map>
 #include <tuple>
 
 // SDL
@@ -22,11 +21,6 @@ protected:
 	unsigned int surface_width = 0;
 	unsigned int surface_height = 0;
 	
-	unsigned int tile_width = 0;
-	unsigned int tile_height = 0;
-
-	std::map<std::string, Animation2D> animations;
-	std::string current_animation;
 
 	SDL_Renderer * renderer = nullptr;
 
@@ -56,42 +50,7 @@ public:
 	std::tuple<int, int> get_width_height() const
 	{
 		return std::make_tuple(surface_width, surface_height);
-	}
-
-	void set_atlas_tiling(unsigned int width, unsigned int height)
-	{
-		tile_width = width;
-		tile_height = height;
-	}
-
-	void add_animation(std::string const & name, Animation2D const & anim)
-	{ 
-		animations.insert(std::pair<std::string, Animation2D>(name, anim));
-	}
-	
-	//void set_current_animation(std::string const & name)
-	//{
-	//	auto anim = &animations.find(name);
-	//	if (anim)
-	//	{
-	//		current_animation = name;
-	//	} // else error?
-	//}
-
-	std::vector<std::string> get_animation_list()
-	{
-		std::vector<std::string> result;
-
-		for (auto&& pair : animations)
-		{
-			result.push_back(pair.first);
-		}
-
-		return result;
-	}
-
-	// std::vector<std::string> get_animations()
-	// SDL_Rect get_next_animation_frame()
+	}	
 };
 
 #endif // RENDER_RESOURCE_H

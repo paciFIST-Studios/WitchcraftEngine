680e3040226ff28a279be19f12932d19dfb2dd1c
ellielove <ellie@pacifist.studio>
2020-08-24 12:57:13 -0700

    Added message bus class

---
+++ witchcraft_sln/witchcraft/message_bus/message_bus.h
@@ -0,0 +1,22 @@
+#ifndef MESSAGE_BUS_H
+#define MESSAGE_BUS_H
+
+#include "../engine/engine_object.h"
+
+class MessageBus : public qEngineObject
+{
+
+
+
+	// subscribe
+	// un-subscribe
+
+	// send message
+	// send interrupting message
+	// send delayed message
+
+};
+
+
+
+#endif // !MESSAGE_BUS_H

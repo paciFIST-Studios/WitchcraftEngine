fe182a39dbbf8505b07a7fa4100781bfa9ab09f6
ellielove <ellie@pacifist.studio>
2020-08-24 18:03:54 -0700

    Updated tests
    1. moved some fns out of the test file, and into a namespace, below the message bus code
    
    2. added message_bus::push_message.  Messages are pushed into a map, where the key is the recipient ID
    
    3. added message_bus::peek_message_count; just counts the number of messages a recipient has
    
    4. added message_bus::get_messages, which returns the whole vector of existing messages, and then puts a new vector in its place
    
    5. current tests cover use of Message struct's void* data pointer
    
    6. Current tests cover message_bus::push_message

--- witchcraft_sln/witchcraft/message_bus/message_bus.h
+++ witchcraft_sln/witchcraft/message_bus/message_bus.h
@@ -1,22 +1,123 @@
 #ifndef MESSAGE_BUS_H
 #define MESSAGE_BUS_H
 
-#include "../engine/engine_object.h"
+#include <functional>
+#include <map>
 
-class MessageBus : public qEngineObject
+#include "../engine/engine_object.h"
+//#include "../message_bus/ICanSendMessages.h"
+//#include "../message_bus/ICanReceiveMessages.h"
+
+enum MessageType
+{
+	  UNINITIALIZED = 0
+	  // used for testing, does not signify specific decoding
+	, TESTING
+};
+
+struct Message
+{
+public:
+	unsigned int message_id;
+	unsigned int recipient;
+	unsigned int sender;
+	MessageType type;
+	void * data;
+};
+
+class MessageBus 
+	: public qEngineObject 
+	//, public ICanSendMessages
+	//, public ICanReceiveMessages
 {
 
+private:
+protected:
 
+	// ID, vec<recipient's messages>
+	std::map <unsigned int, std::vector<Message>> message_map;
+
+	virtual void onMessage(Message m){}
+
+public:
+
+	MessageBus()
+		: message_map({})
+	{}
+
+	~MessageBus()
+	{}
 
 	// subscribe
 	// un-subscribe
 
-	// send message
+	void push_message(Message m)
+	{
+		bool result = false;
+		if (message_map.find(m.recipient) != message_map.end())
+		{
+			message_map[m.recipient].push_back(m);
+		}
+		else // add new key & vector
+		{
+			message_map[m.recipient] = std::vector<Message>{ m };
+		}
+	}
+
+	//bool push_interrupt_message(Message m);
 	// send interrupting message
 	// send delayed message
 
+	int peek_message_count(unsigned int id)
+	{
+		if (message_map.find(id) != message_map.end())
+		{
+			return message_map[id].size();
+		}
+		else
+		{
+			return -1;
+		}
+	}
+
+	std::vector<Message> get_messages(unsigned int id)
+	{
+		std::vector<Message> result;
+
+		for (auto m : message_map[id])
+		{
+			result.push_back(m);
+		}
+		
+		message_map[id] = std::vector<Message>();
+
+		return result;
+	}
+
 };
 
+
+namespace witchcraft
+{
+	namespace testing
+	{
+		struct FourIntStruct
+		{
+			int a = 0;
+			int b = 0;
+			int c = 0;
+			int d = 0;
+		};
+
+		static unsigned int get_times_called()
+		{
+			static unsigned int count = 0;
+			return ++count;
+		}
+
+	}
+
+}
 
 
 #endif // !MESSAGE_BUS_H
--- witchcraft_sln/witchcraft/message_bus/message_bus__tst.cc
+++ witchcraft_sln/witchcraft/message_bus/message_bus__tst.cc
@@ -9,76 +9,96 @@
 
 		#include "message_bus.h"
 
-		TEST_CASE(" message_bus::ctor    creates generic object")
-		{
-			auto mbus = MessageBus();
-			REQUIRE(mbus.id == uninit::UINT);
-		}
-
-		bool test_returns_false_once_then_true_always()
-		{
-			static bool called_twice = false;
-
-			if (called_twice == false)
-			{
-				called_twice = true;
-				return false;
-			}
-
-			return called_twice;
-		}
-
 		TEST_CASE("Message::data    void * can be used to hold a fn ptr")
 		{
 			//https://www.kdab.com/how-to-cast-a-function-pointer-to-a-void/
-			auto fp = &test_returns_false_once_then_true_always;
-			bool r1;
-			REQUIRE_NOTHROW(r1 = fp());
-			REQUIRE(r1 == false);
+			auto fp = &witchcraft::testing::get_times_called;
+			auto a = fp();
+			auto b = fp();
+			REQUIRE_NOTHROW(fp());
+			REQUIRE(a < b);
 
 			auto m = Message{
 				  0	// message id
 				, 1	// recipient
 				, 2	// sender
-				, MessageType::FunctionCall
+				, MessageType::TESTING
 				// cast a fn ptr to a void*
 				, reinterpret_cast<void *&>(fp) // data
 			};
 
-			// cast a void * to a fn ptr
-			auto fp2 = reinterpret_cast<bool(*)()>(m.data);
-			bool r2;
-			REQUIRE_NOTHROW(r2 = fp2());
-			REQUIRE(r2 == true);
+			if (m.type == MessageType::TESTING) 
+			{
+				// cast a void * to a fn ptr
+				auto fp2 = reinterpret_cast<unsigned int(*)(void)>(m.data);
+				auto c = fp2();
+				auto d = fp2();
+
+				REQUIRE_NOTHROW(fp2());
+				REQUIRE(c < d);
+				REQUIRE(a < c);
+				REQUIRE(b < d);
+			}
 		}
 
 
-		struct TestData
-		{
-			int a;
-			int b;
-			int c;
-			int d;
-		};
+
 		TEST_CASE("Message::data    void * can be used to hold object ptr")
 		{
-			auto test_data = TestData{ 1, 3, 5, 7 };
+			using witchcraft::testing::FourIntStruct;
+			auto test_data = FourIntStruct{ 1, 3, 5, 7 };
 
 			auto m = Message{
 				  0	// message id
 				, 1	// recipient
 				, 2 // sender
-				, MessageType::Object
+				, MessageType::TESTING
 				, &test_data 
 			};
 
-			// NOTE: This has to be cast as a ptr again,
-			auto obj = (TestData*)m.data;
+			if (m.type == MessageType::TESTING)
+			{
+				// NOTE: This has to be cast as a ptr again,
+				auto obj = (FourIntStruct*)m.data;
 
-			REQUIRE(test_data.a == obj->a);
-			REQUIRE(test_data.b == obj->b);
-			REQUIRE(test_data.c == obj->c);
-			REQUIRE(test_data.d == obj->d);
+				REQUIRE(test_data.a == obj->a);
+				REQUIRE(test_data.b == obj->b);
+				REQUIRE(test_data.c == obj->c);
+				REQUIRE(test_data.d == obj->d);
+			}
+		}
+
+
+		TEST_CASE(" MessageBus::ctor    creates generic object")
+		{
+			auto mbus = MessageBus();
+			REQUIRE(mbus.id == uninit::UINT);
+		}
+
+
+		TEST_CASE(" MessageBus::push_message    pushes message onto vector   when given correct args")
+		{
+			auto data = witchcraft::testing::FourIntStruct{ 7, 7, 7, 7 };
+
+			int const RECIPIENT_ID = 666;
+
+			auto mb = MessageBus();
+			auto m = Message{
+				  1	// message id
+				, RECIPIENT_ID
+				, 3	// sender
+				, MessageType::TESTING
+				, &data
+			};
+
+			REQUIRE(mb.peek_message_count(RECIPIENT_ID) == -1);
+			mb.push_message(m);
+			REQUIRE(mb.peek_message_count(RECIPIENT_ID) == 1);
+			mb.push_message(m);
+			REQUIRE(mb.peek_message_count(RECIPIENT_ID) == 2);
+			mb.push_message(m);
+			REQUIRE(mb.peek_message_count(RECIPIENT_ID) == 3);
+
 		}
 
 	#endif // RUN_UNIT_TESTS

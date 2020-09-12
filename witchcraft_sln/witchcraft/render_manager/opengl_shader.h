98bd536e8578ca5b7dd2aac20bee1ba708f83689
ellielove <ellie@pacifist.studio>
2020-09-11 19:13:46 -0700

    Shader class code
    1. this isn't commented out, but it's also not hooked up
    
    2. In order to call the open gl fns, we have to have an opengl context.  The context isn't created during the unit test phase.  Our options are basically:
    
    a. create an open GL context in unit test phase, and tear it back down before starting the main program
    
    b. don't run unit tests on shader compilation, and rely on errors to come out correctly in logs)
    
    I'm not sure what to do, so I'm gonna think about it for a while, then decide
    
    3. other work:
    
    a. OpenGLShaderProgram is split into small fns, and I feel good about it overall
    b. class is starting to deviate from the tutorial code I was using to build it, which is also good

--- witchcraft_sln/witchcraft/render_manager/opengl_shader.h
+++ witchcraft_sln/witchcraft/render_manager/opengl_shader.h
@@ -5,22 +5,43 @@
 
 #include <string>
 
+#include <SDL.h>
+#include <GL/glew.h>
+
 #include "../engine/engine_object.h"
 
-class OpenGlShader : public qEngineObject
+class OpenGlShaderProgram : public qEngineObject
 {
 private:
-	unsigned int shader_id;
-
-	void check_compile_error(unsigned int obj, char const * type);
-
 protected:
+	unsigned int shader_program_id = 0;
+
+	bool check_compile_errors(unsigned int id, char const * type);
+
+	// constants used for messaging state inside the class
+	char const * VERTEX		= "VERTEX";
+	char const * FRAGMENT	= "FRAGMENT";
+	char const * GEOMETRY	= "GEOMETRY";
+	char const * PROGRAM	= "PROGRAM";
+
+	GLuint compile_vertex_shader(char const * src);
+	GLuint compile_fragment_shader(char const * src);
+	GLuint compile_geometry_shader(char const * src);
+	GLuint link_program(GLuint vert, GLuint frag, GLuint geo = NULL);
+
 public:
-	OpenGlShader() {}
 
-	unsigned int shader_id() { return shader_id; }
+	OpenGlShaderProgram()
+	{
+		if (glewInit() != GLEW_OK)
+		{
 
-	OpenGlShader & Use();
+		}
+	}
+
+	unsigned int get_shader_program_id() { return shader_program_id; }
+
+	OpenGlShaderProgram & Use();
 
 	void compile(char const * vertex_src, char const * fragment_src, char const * geometry_src = nullptr);
 
--- witchcraft_sln/witchcraft/render_manager/opengl_shader__tst.cc
+++ witchcraft_sln/witchcraft/render_manager/opengl_shader__tst.cc
@@ -9,15 +9,69 @@
 
 		#include "../render_manager/opengl_shader.h"
 		
-		TEST_CASE("")
+		class test_OpenGLShaderProgram : public OpenGlShaderProgram
 		{
-			auto shader = OpenGlShader();
+		public:
+			char const * passing_vert_src = "void main(void){}";
+			char const * failing_vert_src = "";
 
+			char const * passing_frag_src = "";
+			char const * failing_frag_src = "";
+
+			char const * passing_geo_src = "";
+			char const * failing_geo_src = "";
+
+			GLuint test_compile_vertex_shader(char const * src)
+			{
+				return compile_vertex_shader(src);
+			}
+
+			GLuint test_compile_fragment_shader(char const * src)
+			{
+				return compile_fragment_shader(src);
+			}
+
+			GLuint test_compile_geometry_shader(char const * src)
+			{
+				return compile_geometry_shader(src);
+			}
+
+			bool test_link_program(GLuint vert, GLuint frag, GLuint geo = NULL)
+			{
+				return link_program(vert, frag, geo);
+			}
+
+			bool test_no_compile_error(unsigned int obj, char const * type) 
+			{ 
+				return check_compile_errors(obj, type); 
+			}
+
+		};
+
+		TEST_CASE("OpenGLShaderProgram:: ctor  constructs object w/ default params")
+		{
+			auto shader = &OpenGlShaderProgram();
+			REQUIRE(shader);
 		}
 	
-		TEST_CASE("")
-		{
-		}
+		//TEST_CASE("OpenGLShaderProgram:: compiles  passing vert src without error")
+		//{
+		//	auto shader = test_OpenGLShaderProgram();
+		//	auto src = shader.passing_vert_src;
+		//	auto pass = shader.test_compile_vertex_shader(src);
+		//	REQUIRE(pass);
+		//}
+
+		//
+		//TEST_CASE("OpenGLShaderProgram:: shows  compile error for failing vert src")
+		//{
+		//	auto s = test_OpenGLShaderProgram();
+		//	auto pass = s.test_compile_fragment_shader(s.failing_frag_src);
+		//	REQUIRE_FALSE(pass);
+		//}
+		//
+		//TEST_CASE("OpenGLShaderProgram:: compiles  passing frag src without error"){}
+		//TEST_CASE("OpenGLShaderProgram:: shows  compile error for failing frag src"){}
 
 
 	#endif // RUN_UNIT_TESTS

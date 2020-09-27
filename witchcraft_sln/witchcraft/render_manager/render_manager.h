f3a2953f1a541b80f7437d8ecff95f662185f303
ellielove <ellie@pacifist.studio>
2020-09-27 02:01:39 -0700

    Update render_manager.h
    general updates, and clearing out of previous opengl buffers etc

--- witchcraft_sln/witchcraft/render_manager/render_manager.h
+++ witchcraft_sln/witchcraft/render_manager/render_manager.h
@@ -23,8 +23,6 @@
 #include <glm/gtc/matrix_transform.hpp>
 #include <glm/gtc/type_ptr.hpp>
 
-
-
 // rapidxml
 #include "../../lib/rapidxml/rapidxml.hpp"
 #include "../../lib/rapidxml/rapidxml_utils.hpp"
@@ -35,11 +33,10 @@
 #include "../imgui/imgui_impl_sdl.h"
 #include "../imgui/imgui_impl_opengl3.h"
 
+// STB_Image
 #include "../stb_image.h"
 
 // witchcraft
-#include "../engine/engine_object.h"
-
 #include "../message_bus/message_bus.h"
 
 #include "../resource_manager/engine_resource_base.h"
@@ -53,13 +50,12 @@
 
 #include "opengl_shader.h"
 #include "opengl_texture.h"
+#include "../resource_manager/vertex_resource.h"
 
 #include "../console/console.h"
 
 
 class SceneManager2D;
-class qSceneObject;
-class Console;
 
 enum class ERendererState : unsigned char
 {
@@ -73,14 +69,6 @@ enum class ERendererState : unsigned char
 	, SHUTDOWN_OK	= 0x80
 };
 
-struct OpenGLTextureStruct
-{
-	int width;
-	int height;
-	int color_channels;
-	unsigned int id;
-};
-
 class RenderManager : public EngineObjectBase
 {
 public:
@@ -89,125 +77,50 @@ public:
 private:
 	ERendererState renderer_state = ERendererState::UNINITIALIZED;
 
-	std::map<GLenum, std::string> Glenum_to_str{
-		  { GL_VERTEX_SHADER	, "GL_VERTEX_SHADER"	}
-		, { GL_FRAGMENT_SHADER	, "GL_FRAGMENT_SHADER"	}
-	};
-
 	char const * open_gl_version = "#version 330 core";
 
 	char const * basic_vertex_src =
 		"#version 330 core\n"
-		"layout(location=0) in vec3 _pos;\n"
-		"layout(locaiton=1) in vec2 _uv;\n"
-		"out vec2 uv;\n"
+		"layout(location=0) in vec2 _pos;\n"
+		"layout(location=1) in vec2 _ts;\n"
+		"out vec2 ts;\n"
 		"void main(){\n"
-		"gl_Position = vec4(_pos, 1.0f);\n"
-		"uv = _uv;\n}"
+		"gl_Position = vec4(_pos.x, _pos.y, 0.0f, 1.0f);\n"
+		"ts = _ts;\n}"
 		;
 
 	char const * basic_fragment_src =
 		"#version 330 core\n"
 		"out vec4 color;\n"
-		"in vec2 uv;\n"
+		"in vec2 ts;\n"
 		"uniform sampler2D _texture;\n"
 		"void main(){\n"
-		"color = texture(_texture, uv);\n}"
+		"color = texture(_texture, ts);\n}"
 		;
 
+	//	vao
+	//	vbo
+	//	ebo
+	//  texture
+	std::vector<GLuint> sprite_quad;
 
-	char const * basic_perspective_vertex_shader_src =
-		"#version 330 core\n"
-		"layout(location=0) in vec3 _pos;\n"
-		"layout(location=1) in vec2 _uv_tex;\n"
-		"out vec2 uv_tex;\n"
-		"uniform mat4 model;\n"
-		"uniform mat4 view;\n"
-		"uniform mat4 projection;\n"
-		"void main(){\n"
-		"gl_Position = projection * view * model * vec4(_pos, 1.0f);\n"
-		"uv_tex = vec2(_uv_tex.x, _uv_tex.y);\n}"
-		;
+	GLuint sprite_quad_vao;
+	GLuint sprite_quad_vbo;
+	GLuint sprite_quad_ebo;
 
-	char const * basic_perspective_fragment_shader_src = 
-		"#version 330 core\n"
-		"out vec4 frag_color;\n"
-		"in vec2 uv_tex;\n"
-		"uniform sampler2D tex;\n"
-		"void main(){\n"
-		"frag_color = texture(tex, uv_tex);\n}"		
-		;
-
-	char const * sprite_vertex_shader_src =
-		"#version 330 core\n"
-		"layout(location=0) in vec3 vertex;\n"
-		"layout(location=1) in vec2 _uv;\n" // texture coordinate
-		"layout(location=2) in vec2 _wh;\n" // screen width and height
-		"out vec2 uv;\n"
-		"void main(){\n"
-		"uv = _uv;\n"
-		"gl_Position = vec4(vertex.x / wh.x -1.0, vertex.y / wh.h - 1.0, 0.0, 1.0);}\n";
-
-	char const * sprite_fragment_shader_src =
-		"#version 330 core\n"
-		"out vec4 color;\n"
-		"in vec2 uv;\n"
-		"uniform sampler2D tex;"
-		"void main(){"
-		"color = texture(tex, uv);\n}\n";
-
-	OpenGLTexture * sprite_tex;
-	OpenGLTextureStruct sprite_texture;
+	OpenGLTexture sprite_texture;
+	
 	glm::mat4 model_matrix;
 	glm::mat4 view_matrix;
 	glm::mat4 orthographic_projection_matrix;
 	glm::mat4 perspective_projection_matrix;
 
-	GLfloat const triangle_verticies[9] = {
-		//  x	   y	z
-		 -0.5f, -0.5f, 0.0f		// 0
-		, 0.5f, -0.5f, 0.0f		// 1
-		, 0.0f,  0.5f, 0.0f		// 2
-	};
-	
-	GLfloat const sprite_verticies[12] = {
-		   0.5f,  0.5f, 0.0f // top right
-		,  0.5f, -0.5f, 0.0f // bottom right
-		, -0.5f, -0.5f, 0.0f // bottom left
-		, -0.5f,  0.5f, 0.0f // top left
-	};
-	GLuint const sprite_indicies[6] = {
-		  0, 1, 3 // t1
-		, 1, 2, 3 // t2
-	};
-
-	GLfloat const unified_sprite_verts[32] = {
-		  // pos				// color			// uv
-		  0.5f, 0.5f, 0.0f,		1.0f, 0.0f, 0.0f,	1.0f, 1.0f	// top right
-		, 0.5f,-0.5f, 0.0f,		0.0f, 1.0f, 0.0f,	1.0f, 0.0f	// bttm right
-		,-0.5f,-0.5f, 0.0f,		0.0f, 0.0f, 1.0f,	0.0f, 0.0f	// bttm left
-		,-0.5f, 0.5f, 0.0f,		1.0f, 1.0f, 0.0f,	0.0f, 1.0f	// top left
-	};
-	GLuint const unified_sprite_indices[6] = {
-		  0, 1, 3
-		, 1, 2, 3
-	};
-
-	GLuint sprite_quad_vao;	// vertex array object
-	GLuint sprite_quad_vbo;	// vertex buffer object
-	GLuint sprite_quad_ebo; // element buffer object
-
-	GLuint unified_sprite_quad_vao;
-	GLuint unified_sprite_quad_vbo;
-	GLuint unified_sprite_quad_ebo;
-
-	glm::mat4 projection_matrix;
 
 	bool use_wireframe_rendering = false;
 	bool draw_triangle_not_quad = false;
-
 	bool draw_imgui_main_menu_bar = true;
 	bool draw_imgui_debug_window = false;
+
 
 	void paint_imgui_main_menu_bar();
 
@@ -245,15 +158,8 @@ protected:
 	unsigned int screen_width = 0;
 	unsigned int screen_height = 0;
 
-	// stores info on state of vertex data, (& format)
-	GLuint triangle_vao;
-
-	// stores the vertex data
-	GLuint triangle_vbo;
-
 	bool init_sdl(unsigned xOffset, unsigned yOffset, unsigned Width, unsigned Height, char const * WindowTitle);	
 	bool init_opengl();
-	bool init_sdl_image();
 
 	bool init_shaders();
 	bool init_geometry();
@@ -273,6 +179,7 @@ protected:
 
 	unsigned int engine_channel_id = 0;
 	unsigned int render_channel_id = 0;
+	unsigned int resource_channel_id = 0;
 
 
 public:
@@ -290,6 +197,7 @@ public:
 		// cache these locally, b/c we're likely to use them very often
 		render_channel_id = message_bus->channel_lookup("render");
 		engine_channel_id = message_bus->channel_lookup("engine");
+		resource_channel_id = message_bus->channel_lookup("resource");
 	}
 
 	bool init_system(

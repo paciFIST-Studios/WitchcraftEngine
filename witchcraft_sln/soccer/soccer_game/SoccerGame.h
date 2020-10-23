248d4adf3172410f3caddc915476e1318553dd2d
ellielove <ellie@pacifist.studio>
2020-10-23 12:38:47 -0700

    Update SoccerGame.h

--- witchcraft_sln/soccer/soccer_game/SoccerGame.h
+++ witchcraft_sln/soccer/soccer_game/SoccerGame.h
@@ -1,11 +1,21 @@
 #ifndef SOCCER_GAME_H
 #define SOCCER_GAME_H
 
-class SoccerGame
+#include "../../witchcraft/gameplay_manager/game.h"
+
+class SoccerGame : public GameBase
 {
 private:
 protected:
 public:
+
+	// demo movie screen, press start to being
+		// quickstart -> launch into game using current profile, w/ random teams and stadium game starts within 15s
+		// set up new game -> choose teams, stadium, add new player profile
+		// settings -> 
+			// tutorial
+		// exit program
+		// after no input for 60s, go to demo
 
 };
 
